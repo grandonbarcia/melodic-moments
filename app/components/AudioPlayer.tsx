15f8ec97263d48e2a0f4085c6d38ce8ca22914e8
Brandon Garcia <grandonleebarcia@gmail.com>
2025-05-16 15:09:52 -0700

    Fetch list of songs from S3 Bucket and display them

--- app/components/AudioPlayer.tsx
+++ app/components/AudioPlayer.tsx
@@ -9,20 +9,30 @@ import { FaStepBackward } from 'react-icons/fa';
 import { useEffect, useRef, useState } from 'react';
 import getListOfObjects from '../actions/actions';
 
+const API = `https://melodic-moments.s3.us-east-2.amazonaws.com/`;
+
 export default function AudioPlayer() {
   const [isPlaying, setIsPlaying] = useState(false);
   const [currentTime, setCurrentTime] = useState(0);
   const [duration, setDuration] = useState(0);
+  const [listOfSongs, setListOfSongs] = useState<string[]>([]);
+  const [currSong, setCurrSong] = useState<string | null>(null);
   const audioRef = useRef<HTMLAudioElement | null>(null);
 
   useEffect(() => {
-    getListOfObjects()
-      .then((data) => {
-        console.log('Fetched objects:', data);
-      })
-      .catch((error) => {
+    const fetchObjects = async () => {
+      try {
+        const data = await getListOfObjects();
+        const filteredData = data.filter(
+          (item): item is string => typeof item === 'string'
+        );
+        setListOfSongs(filteredData);
+        setCurrSong(API + filteredData[0]);
+      } catch (error) {
         console.error('Error fetching objects:', error);
-      });
+      }
+    };
+    fetchObjects();
   }, []);
 
   const togglePlay = () => {
@@ -36,6 +46,15 @@ export default function AudioPlayer() {
     }
     setIsPlaying(!isPlaying);
   };
+
+  useEffect(() => {
+    const audio = audioRef.current;
+    if (isPlaying) {
+      if (audio) {
+        audio.play();
+      }
+    }
+  }, [currSong, isPlaying]);
 
   const handleTimeUpdate = () => {
     const audio = audioRef.current;
@@ -62,7 +81,7 @@ export default function AudioPlayer() {
   return (
     <div className="flex flex-col gap-16 relative mx-auto mt-6 max-w-xl">
       <audio
-        src="./song.wav"
+        src={currSong ?? undefined}
         ref={audioRef}
         onTimeUpdate={handleTimeUpdate}
         onLoadedMetadata={handleLoadedMetadata}
@@ -84,6 +103,21 @@ export default function AudioPlayer() {
           max={duration}
           onValueChange={(value) => handleSliderChange(value[0])}
         />
+      </div>
+      <div>
+        {listOfSongs.map((song, index) => (
+          <div key={index} className="flex items-center justify-between">
+            <span>{song}</span>
+            <Button
+              onClick={() => {
+                setIsPlaying(true);
+                setCurrSong(API + song);
+              }}
+            >
+              Play
+            </Button>
+          </div>
+        ))}
       </div>
     </div>
   );
